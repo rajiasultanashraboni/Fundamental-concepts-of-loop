6b6f2a6c587a22bdfeead765def9cde28f3eb287
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-08 14:21:58 +0600

    find even number using while loop is here

---
+++ while.js
@@ -0,0 +1,13 @@
+let num = 1;
+while (num <= 10) {
+  //   console.log(num);
+  //   num = num + 1;
+  num += 1;
+  //   num++;
+}
+
+let num2 = 50;
+while (num2 <= 100) {
+  console.log(num2);
+  num2++;
+}

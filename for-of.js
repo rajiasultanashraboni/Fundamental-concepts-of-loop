5e431ca915e75cc375f55a1619555afddfd43b90
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-08 13:52:02 +0600

    while loop will keep running as long as the condition is true

--- for-of.js
+++ for-of.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+const fruits = ["Mango", "banana", "apple", "jambura", "pine-aplle"];
+
+console.log(fruits);
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
---
+++ while-loop.js
@@ -0,0 +1,12 @@
+const number = 7;
+if (number < 10) {
+  // console.log("this the number");
+}
+
+// console.log("this is not a number");
+
+let num = 0;
+while (num < 20000) {
+  console.log("Sorry", num);
+  num = num + 1;
+}

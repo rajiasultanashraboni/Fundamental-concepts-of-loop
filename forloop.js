f5b92b977612549d54bf5a11c92de99c5bc6f4bd
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-08 20:11:46 +0600

    find odd and even number using for loop is here

---
+++ forloop.js
@@ -0,0 +1,16 @@
+// odd number
+for (let i = 1; i <= 20; i = i + 2) {
+  //   console.log("odd", i);
+}
+
+// even number
+
+for (let i = 0; i <= 20; i = i + 2) {
+  console.log("even", i);
+}
+
+// print sorry using for loop
+
+for (let i = 0; i <= 20; i++) {
+  console.log("sorryyyyyyyy", i);
+}

af78832fe9c7ff474c1ccac830ef46bee53c1c5f
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-08 21:51:48 +0600

    different ways of loop is here

---
+++ different-ways.js
@@ -0,0 +1,15 @@
+// find 1 to 100 which is divisible by 5
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}
+
+// find 1 to 100 which is divisible by 3
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+}

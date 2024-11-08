22e9c975cba514bbe06c7cab67cdf337e297ea74
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-08 21:24:51 +0600

    The sum of all even numbers from 51 to 85

--- task3.js
+++ task3.js
@@ -6,9 +6,25 @@ Display sum of all the odd numbers from 91 to 129.
 
  */
 
-let sum = 0;
+// let sum = 0;
 for (let i = 91; i <= 129; i += 2) {
-  console.log(i);
-  sum = sum + i;
+  //   console.log(i);
+  //   sum = sum + i;
 }
-console.log("sum of all the odd numbers from 91 to 129:", sum);
+// console.log("sum of all the odd numbers from 91 to 129:", sum);
+
+// Subtask-2:
+
+// Display sum of all the even numbers from 51 to 85.
+
+//  */
+
+let sum = 0;
+
+for (let num = 51; num <= 85; num++) {
+  if (num % 2 === 0) {
+    console.log(num);
+    sum += num;
+  }
+}
+console.log("The sum of all even numbers from 51 to 85 is:", sum);
